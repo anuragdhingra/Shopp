5b5a0f2d656d0f1d64a9cd679c6155601a8bd084
risingone <anuragdhingra101@gmail.com>
2017-01-15 17:44:10 +0530

    Removed unneccessary files

---
+++ public/js/jsss.js
@@ -0,0 +1,13 @@
+
+
+function fadeit() {
+$(document).ready (function(){
+            $("#success-alert").hide();
+            $("#myWish").click(function showAlert() {
+                $("#success-alert").alert();
+                $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
+               $("#success-alert").alert('close');
+                });   
+            });
+ });
+}
